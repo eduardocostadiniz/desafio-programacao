6eae709299117d3ea6b89cea920bc79d3fdae93e
Eduardo Diniz <eduardocostadiniz67@gmail.com>
2024-09-05 14:37:00 -0300

    feat: Configurando a estrutura inicial
    Configurando a conexão com o PostgreSQL
    Configurando o Flyway

---
+++ src/main/java/com/eduardo/desafio_programacao/DesafioProgramacaoApplication.java
@@ -0,0 +1,13 @@
+package com.eduardo.desafio_programacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioProgramacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioProgramacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduardo/desafio_programacao/DesafioProgramacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.desafio_programacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioProgramacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
